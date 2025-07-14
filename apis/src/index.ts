046ce6db8c68b012f94548c9ee72740bc094f7d4
Tal Vaizman <tal@comitylabs.com>
2025-07-14 19:34:12 +0300

    fix maker psm listener

--- apis/src/index.ts
+++ apis/src/index.ts
@@ -1,5 +1,5 @@
 import { eq } from "drizzle-orm";
-import { dexTrade } from "./db/schema/Listener"; // Adjust the import path as necessary
+//import { dexTrade } from "./db/schema/Listener"; // Adjust the import path as necessary
 import { types, db, App, middlewares } from "@duneanalytics/sim-idx"; // Import schema to ensure it's registered
 
 const app = App.create();
@@ -7,15 +7,15 @@ app.use("*", middlewares.authentication);
 
 app.get("/*", async (c) => {
   try {
-    const result = await db
-      .client(c)
-      .select()
-      .from(dexTrade)
-      .limit(5);
+    // const result = await db
+    //   .client(c)
+    //   .select()
+    //   .from(dexTrade)
+    //   .limit(5);
 
-    return Response.json({
-      result: result,
-    });
+    // return Response.json({
+    //   result: result,
+    // });
   } catch (e) {
     console.error("Database operation failed:", e);
     return Response.json({ error: (e as Error).message }, { status: 500 });
--- listeners/src/Main.sol
+++ listeners/src/Main.sol
@@ -3,12 +3,82 @@ pragma solidity ^0.8.13;
 
 import "sim-idx-sol/Simidx.sol";
 import "sim-idx-generated/Generated.sol";
-import "./MakerPSM.sol";
+import "./types/DexTrades.sol";
+import "./libs/Maker/MakerLib.sol";
+import "./utils/ERC20Metadata.sol";
 
 contract Triggers is BaseTriggers {
     function triggers() external virtual override {
-        MakerPSMListener psmListener = new MakerPSMListener();
+        Listener psmListener = new Listener();
         addTrigger(chainAbi(Chains.Ethereum, PSM$Abi()), psmListener.triggerOnBuyGemFunction());
         addTrigger(chainAbi(Chains.Ethereum, PSM$Abi()), psmListener.triggerOnSellGemFunction());
+    }
+}
+
+contract Listener is PSM$OnBuyGemFunction, PSM$OnSellGemFunction {
+    event DexTrade(DexTradeData);
+
+    function onBuyGemFunction(
+        FunctionContext memory ctx,
+        PSM$BuyGemFunctionInputs memory inputs
+    ) external override {
+        address fromToken = MakerUtils.getDai(ctx.txn.call.callee);
+        address toToken = MakerUtils.getGem(ctx.txn.call.callee);
+        uint256 daiAmt = MakerUtils.getDaiAmt(ctx.txn.call.callee, inputs.gemAmt, false);
+        (string memory fromTokenName, string memory fromTokenSymbol, uint256 fromTokenDecimals) =
+            getMetadata(fromToken);
+        (string memory toTokenName, string memory toTokenSymbol, uint256 toTokenDecimals) = getMetadata(toToken);
+        DexTradeData memory trade = DexTradeData({
+            chainId: uint64(block.chainid),
+            blockNumber: block.number,
+            blockTimestamp: block.timestamp,
+            transactionHash: ctx.txn.hash,
+            dex: "MakerPSM",
+            fromToken: fromToken,
+            fromTokenAmt: daiAmt,
+            fromTokenName: fromTokenName,
+            fromTokenSymbol: fromTokenSymbol,
+            fromTokenDecimals: uint8(fromTokenDecimals),
+            toToken: toToken,
+            toTokenAmt: inputs.gemAmt,
+            toTokenName: toTokenName,
+            toTokenSymbol: toTokenSymbol,
+            toTokenDecimals: uint8(toTokenDecimals),
+            txnOriginator: tx.origin,
+            recipient: inputs.usr,
+            liquidityPool: ctx.txn.call.callee
+        });
+    }
+
+    function onSellGemFunction(
+        FunctionContext memory ctx,
+        PSM$SellGemFunctionInputs memory inputs
+    ) external override {
+        address toToken = MakerUtils.getDai(ctx.txn.call.callee);
+        address fromToken = MakerUtils.getGem(ctx.txn.call.callee);
+        uint256 daiAmt = MakerUtils.getDaiAmt(ctx.txn.call.callee, inputs.gemAmt, true);
+        (string memory fromTokenName, string memory fromTokenSymbol, uint256 fromTokenDecimals) =
+            getMetadata(fromToken);
+        (string memory toTokenName, string memory toTokenSymbol, uint256 toTokenDecimals) = getMetadata(toToken);
+        DexTradeData memory trade = DexTradeData({
+            chainId: uint64(block.chainid),
+            blockNumber: block.number,
+            blockTimestamp: block.timestamp,
+            transactionHash: ctx.txn.hash,
+            dex: "MakerPSM",
+            fromToken: fromToken,
+            fromTokenAmt: inputs.gemAmt,
+            fromTokenName: fromTokenName,
+            fromTokenSymbol: fromTokenSymbol,
+            fromTokenDecimals: uint8(fromTokenDecimals),
+            toToken: toToken,
+            toTokenAmt: daiAmt,
+            toTokenName: toTokenName,
+            toTokenSymbol: toTokenSymbol,
+            toTokenDecimals: uint8(toTokenDecimals),
+            txnOriginator: tx.origin,
+            recipient: inputs.usr,
+            liquidityPool: ctx.txn.call.callee
+        });
     }
 }
