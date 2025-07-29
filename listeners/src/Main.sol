35a08ae19e96513f33e3e5d4d2cdeed7ec5c4c5c
Tal Vaizman <tal@comitylabs.com>
2025-07-29 12:51:23 +0300

    use enumerable maps for protocol configs

--- listeners/src/Main.sol
+++ listeners/src/Main.sol
@@ -25,8 +25,12 @@ import "./MaverickV2.sol";
 import "./Ekubo.sol";
 import "./0xProtocol.sol";
 import "./0xSettler.sol";
+import {ChainsEnumerableMapLib} from "./utils/ChainsEnumerableMapLib.sol";
 
 contract Triggers is BaseTriggers {
+    using ChainsEnumerableMapLib for ChainsEnumerableMapLib.ChainsToChainIdContractMap;
+    using ChainsEnumerableMapLib for ChainsEnumerableMapLib.ChainsToChainIdAbiMap;
+
     MakerPSMListener psmListener;
     UniswapV2Listener uniswapV2Listener;
     UniswapV3Listener uniswapV3Listener;
@@ -54,13 +58,15 @@ contract Triggers is BaseTriggers {
     struct ProtocolConfigAddress {
         Chains[] chains;
         Trigger[] triggers;
-        mapping(Chains => ChainIdContract) chainIdToAddress;
+        //mapping(Chains => ChainIdContract) chainIdToAddress;
+        ChainsEnumerableMapLib.ChainsToChainIdContractMap chainIdToAddressEnumerable;
     }
 
     struct ProtocolConfigAbi {
         Chains[] chains;
         Trigger[] triggers;
-        mapping(Chains => ChainIdAbi) chainIdToAbi;
+        //mapping(Chains => ChainIdAbi) chainIdToAbi;
+        ChainsEnumerableMapLib.ChainsToChainIdAbiMap chainIdToAbiEnumerable;
     }
 
     ProtocolConfigAbi internal psmConfig;
@@ -114,99 +120,103 @@ contract Triggers is BaseTriggers {
         zeroExSettlerListener = new ZeroExSettlerListener();
 
         // address resolving
-        bancorCarbonConfig.chainIdToAddress[Chains.Ethereum] =
-            chainContract(Chains.Ethereum, 0xC537e898CD774e2dCBa3B14Ea6f34C93d5eA45e1);
-        bancorCarbonConfig.chainIdToAddress[Chains.Base] =
-            chainContract(Chains.Base, 0xA4682A2A5Fe02feFF8Bd200240A41AD0E6EaF8d5);
-        gpv2SettlementConfig.chainIdToAddress[Chains.Ethereum] =
-            chainContract(Chains.Ethereum, 0x9008D19f58AAbD9eD0D60971565AA8510560ab41);
-        gpv2SettlementConfig.chainIdToAddress[Chains.Base] =
-            chainContract(Chains.Base, 0x9008D19f58AAbD9eD0D60971565AA8510560ab41);
-        uniswapV4Config.chainIdToAddress[Chains.Unichain] =
-            chainContract(Chains.Unichain, 0x1F98400000000000000000000000000000000004);
-        uniswapV4Config.chainIdToAddress[Chains.Zora] =
-            chainContract(Chains.Zora, 0x0575338e4C17006aE181B47900A84404247CA30f);
-        uniswapV4Config.chainIdToAddress[Chains.WorldChain] =
-            chainContract(Chains.WorldChain, 0xb1860D529182ac3BC1F51Fa2ABd56662b7D13f33);
-        uniswapV4Config.chainIdToAddress[Chains.Ink] =
-            chainContract(Chains.Ink, 0x360E68faCcca8cA495c1B759Fd9EEe466db9FB32);
-        uniswapV4Config.chainIdToAddress[Chains.Soneium] =
-            chainContract(Chains.Soneium, 0x360E68faCcca8cA495c1B759Fd9EEe466db9FB32);
-        airSwapV4Config.chainIdToAddress[Chains.Ethereum] =
-            chainContract(Chains.Ethereum, 0xd82FA167727a4dc6D6F55830A2c47aBbB4b3a0F8);
-        airSwapV5Config.chainIdToAddress[Chains.Ethereum] =
-            chainContract(Chains.Ethereum, 0xD82E10B9A4107939e55fCCa9B53A9ede6CF2fC46);
-        kyberSwapLOPConfig.chainIdToAddress[Chains.Ethereum] =
-            chainContract(Chains.Ethereum, 0x227B0c196eA8db17A665EA6824D972A64202E936);
-        kyberSwapDSLOPConfig.chainIdToAddress[Chains.Ethereum] =
-            chainContract(Chains.Ethereum, 0xcab2FA2eeab7065B45CBcF6E3936dDE2506b4f6C);
-        kyberSwapLOPConfig.chainIdToAddress[Chains.Base] =
-            chainContract(Chains.Base, 0x227B0c196eA8db17A665EA6824D972A64202E936);
-        kyberSwapDSLOPConfig.chainIdToAddress[Chains.Base] =
-            chainContract(Chains.Base, 0xcab2FA2eeab7065B45CBcF6E3936dDE2506b4f6C);
-        ekuboConfig.chainIdToAddress[Chains.Ethereum] =
-            chainContract(Chains.Ethereum, 0xe0e0e08A6A4b9Dc7bD67BCB7aadE5cF48157d444);
-        zeroExProtocolConfig.chainIdToAddress[Chains.Ethereum] =
-            chainContract(Chains.Ethereum, 0xDef1C0ded9bec7F1a1670819833240f027b25EfF);
-        oneInchLOPV4Config.chainIdToAddress[Chains.Ethereum] =
-            chainContract(Chains.Ethereum, 0x111111125421cA6dc452d289314280a0f8842A65);
-        oneInchLOPV4Config.chainIdToAddress[Chains.Base] =
-            chainContract(Chains.Base, 0x111111125421cA6dc452d289314280a0f8842A65);
+        bancorCarbonConfig.chainIdToAddressEnumerable.set(
+            Chains.Ethereum, chainContract(Chains.Ethereum, 0xC537e898CD774e2dCBa3B14Ea6f34C93d5eA45e1)
+        );
+        bancorCarbonConfig.chainIdToAddressEnumerable.set(
+            Chains.Base, chainContract(Chains.Base, 0xA4682A2A5Fe02feFF8Bd200240A41AD0E6EaF8d5)
+        );
+        gpv2SettlementConfig.chainIdToAddressEnumerable.set(
+            Chains.Ethereum, chainContract(Chains.Ethereum, 0x9008D19f58AAbD9eD0D60971565AA8510560ab41)
+        );
+        gpv2SettlementConfig.chainIdToAddressEnumerable.set(
+            Chains.Base, chainContract(Chains.Base, 0x9008D19f58AAbD9eD0D60971565AA8510560ab41)
+        );
+        uniswapV4Config.chainIdToAddressEnumerable.set(
+            Chains.Unichain, chainContract(Chains.Unichain, 0x1F98400000000000000000000000000000000004)
+        );
+        uniswapV4Config.chainIdToAddressEnumerable.set(
+            Chains.Zora, chainContract(Chains.Zora, 0x0575338e4C17006aE181B47900A84404247CA30f)
+        );
+        uniswapV4Config.chainIdToAddressEnumerable.set(
+            Chains.WorldChain, chainContract(Chains.WorldChain, 0xb1860D529182ac3BC1F51Fa2ABd56662b7D13f33)
+        );
+        uniswapV4Config.chainIdToAddressEnumerable.set(
+            Chains.Ink, chainContract(Chains.Ink, 0x360E68faCcca8cA495c1B759Fd9EEe466db9FB32)
+        );
+        uniswapV4Config.chainIdToAddressEnumerable.set(
+            Chains.Soneium, chainContract(Chains.Soneium, 0x360E68faCcca8cA495c1B759Fd9EEe466db9FB32)
+        );
+        uniswapV4Config.chainIdToAddressEnumerable.set(
+            Chains.Base, chainContract(Chains.Base, 0x498581fF718922c3f8e6A244956aF099B2652b2b)
+        );
+        uniswapV4Config.chainIdToAddressEnumerable.set(
+            Chains.Ethereum, chainContract(Chains.Ethereum, 0x000000000004444c5dc75cB358380D2e3dE08A90)
+        );
+        airSwapV4Config.chainIdToAddressEnumerable.set(
+            Chains.Ethereum, chainContract(Chains.Ethereum, 0xd82FA167727a4dc6D6F55830A2c47aBbB4b3a0F8)
+        );
+        airSwapV5Config.chainIdToAddressEnumerable.set(
+            Chains.Ethereum, chainContract(Chains.Ethereum, 0xD82E10B9A4107939e55fCCa9B53A9ede6CF2fC46)
+        );
+        kyberSwapLOPConfig.chainIdToAddressEnumerable.set(
+            Chains.Ethereum, chainContract(Chains.Ethereum, 0x227B0c196eA8db17A665EA6824D972A64202E936)
+        );
+        kyberSwapDSLOPConfig.chainIdToAddressEnumerable.set(
+            Chains.Ethereum, chainContract(Chains.Ethereum, 0xcab2FA2eeab7065B45CBcF6E3936dDE2506b4f6C)
+        );
+        kyberSwapLOPConfig.chainIdToAddressEnumerable.set(
+            Chains.Base, chainContract(Chains.Base, 0x227B0c196eA8db17A665EA6824D972A64202E936)
+        );
+        kyberSwapDSLOPConfig.chainIdToAddressEnumerable.set(
+            Chains.Base, chainContract(Chains.Base, 0xcab2FA2eeab7065B45CBcF6E3936dDE2506b4f6C)
+        );
+        ekuboConfig.chainIdToAddressEnumerable.set(
+            Chains.Ethereum, chainContract(Chains.Ethereum, 0xe0e0e08A6A4b9Dc7bD67BCB7aadE5cF48157d444)
+        );
+        zeroExProtocolConfig.chainIdToAddressEnumerable.set(
+            Chains.Ethereum, chainContract(Chains.Ethereum, 0xDef1C0ded9bec7F1a1670819833240f027b25EfF)
+        );
+        zeroExProtocolConfig.chainIdToAddressEnumerable.set(
+            Chains.Base, chainContract(Chains.Base, 0xDef1C0ded9bec7F1a1670819833240f027b25EfF)
+        );
+        oneInchLOPV4Config.chainIdToAddressEnumerable.set(
+            Chains.Ethereum, chainContract(Chains.Ethereum, 0x111111125421cA6dc452d289314280a0f8842A65)
+        );
+        oneInchLOPV4Config.chainIdToAddressEnumerable.set(
+            Chains.Base, chainContract(Chains.Base, 0x111111125421cA6dc452d289314280a0f8842A65)
+        );
 
         // protocol to abi
-        psmConfig.chainIdToAbi[Chains.Ethereum] = chainAbi(Chains.Ethereum, PSM$Abi());
-        univ2Config.chainIdToAbi[Chains.Ethereum] = chainAbi(Chains.Ethereum, UniswapV2Pair$Abi());
-        univ2Config.chainIdToAbi[Chains.Base] = chainAbi(Chains.Base, UniswapV2Pair$Abi());
-        univ3Config.chainIdToAbi[Chains.Ethereum] = chainAbi(Chains.Ethereum, UniswapV3Pool$Abi());
-        univ3Config.chainIdToAbi[Chains.Base] = chainAbi(Chains.Base, UniswapV3Pool$Abi());
-        crocSwapConfig.chainIdToAbi[Chains.Ethereum] = chainAbi(Chains.Ethereum, HotProxy$Abi());
-        crocSwapConfig.chainIdToAbi[Chains.Base] = chainAbi(Chains.Base, HotProxy$Abi());
-        curveOldExchangeConfig.chainIdToAbi[Chains.Ethereum] = chainAbi(Chains.Ethereum, OldTokenExchange$Abi());
-        curveExchangeConfig.chainIdToAbi[Chains.Ethereum] = chainAbi(Chains.Ethereum, TokenExchange$Abi());
-        curveOldExchangeConfig.chainIdToAbi[Chains.Base] = chainAbi(Chains.Base, OldTokenExchange$Abi());
-        curveExchangeConfig.chainIdToAbi[Chains.Base] = chainAbi(Chains.Base, TokenExchange$Abi());
-        balancerV2Config.chainIdToAbi[Chains.Ethereum] = chainAbi(Chains.Ethereum, Vault$Abi());
-        balancerV2Config.chainIdToAbi[Chains.Base] = chainAbi(Chains.Base, Vault$Abi());
-        maverickV1Config.chainIdToAbi[Chains.Ethereum] = chainAbi(Chains.Ethereum, MaverickPool$Abi());
-        maverickV1Config.chainIdToAbi[Chains.Base] = chainAbi(Chains.Base, MaverickPool$Abi());
-        uniswapXConfig.chainIdToAbi[Chains.Ethereum] = chainAbi(Chains.Ethereum, Reactor$Abi());
-        uniswapXConfig.chainIdToAbi[Chains.Base] = chainAbi(Chains.Base, Reactor$Abi());
-        uniswapXConfig.chainIdToAbi[Chains.Unichain] = chainAbi(Chains.Unichain, Reactor$Abi());
-        dodoV2Config.chainIdToAbi[Chains.Ethereum] = chainAbi(Chains.Ethereum, DODOSwap$Abi());
-        dodoV2Config.chainIdToAbi[Chains.Base] = chainAbi(Chains.Base, DODOSwap$Abi());
-        wooFiConfig.chainIdToAbi[Chains.Ethereum] = chainAbi(Chains.Ethereum, WooSwap$Abi());
-        wooFiConfig.chainIdToAbi[Chains.Base] = chainAbi(Chains.Base, WooSwap$Abi());
-        fluidDexConfig.chainIdToAbi[Chains.Ethereum] = chainAbi(Chains.Ethereum, FluidDexT1$Abi());
-        fluidDexConfig.chainIdToAbi[Chains.Base] = chainAbi(Chains.Base, FluidDexT1$Abi());
-        maverickV2Config.chainIdToAbi[Chains.Ethereum] = chainAbi(Chains.Ethereum, MaverickV2Pool$Abi());
-        maverickV2Config.chainIdToAbi[Chains.Base] = chainAbi(Chains.Base, MaverickV2Pool$Abi());
-        zeroExSettlerConfig.chainIdToAbi[Chains.Ethereum] = chainAbi(Chains.Ethereum, MainnetSettler$Abi());
-
-        // per protocol supported chains
-        univ2Config.chains = [Chains.Ethereum, Chains.Base];
-        univ3Config.chains = [Chains.Ethereum, Chains.Base];
-        bancorCarbonConfig.chains = [Chains.Ethereum, Chains.Base];
-        gpv2SettlementConfig.chains = [Chains.Ethereum, Chains.Base];
-        crocSwapConfig.chains = [Chains.Ethereum, Chains.Base];
-        curveOldExchangeConfig.chains = [Chains.Ethereum, Chains.Base];
-        curveExchangeConfig.chains = [Chains.Ethereum, Chains.Base];
-        balancerV2Config.chains = [Chains.Ethereum, Chains.Base];
-        maverickV1Config.chains = [Chains.Ethereum, Chains.Base];
-        uniswapXConfig.chains = [Chains.Ethereum, Chains.Base, Chains.Unichain];
-        uniswapV4Config.chains =
-            [Chains.Ethereum, Chains.Unichain, Chains.Base, Chains.Zora, Chains.WorldChain, Chains.Ink, Chains.Soneium];
-        dodoV2Config.chains = [Chains.Ethereum, Chains.Base];
-        wooFiConfig.chains = [Chains.Ethereum, Chains.Base];
-        airSwapV4Config.chains = [Chains.Ethereum];
-        airSwapV5Config.chains = [Chains.Ethereum];
-        fluidDexConfig.chains = [Chains.Ethereum, Chains.Base];
-        oneInchLOPV4Config.chains = [Chains.Ethereum, Chains.Base];
-        kyberSwapLOPConfig.chains = [Chains.Ethereum, Chains.Base];
-        kyberSwapDSLOPConfig.chains = [Chains.Ethereum, Chains.Base];
-        maverickV2Config.chains = [Chains.Ethereum, Chains.Base];
-        ekuboConfig.chains = [Chains.Ethereum];
-        zeroExProtocolConfig.chains = [Chains.Ethereum, Chains.Base];
-        zeroExSettlerConfig.chains = [Chains.Ethereum];
+        psmConfig.chainIdToAbiEnumerable.set(Chains.Ethereum, chainAbi(Chains.Ethereum, PSM$Abi()));
+        univ2Config.chainIdToAbiEnumerable.set(Chains.Ethereum, chainAbi(Chains.Ethereum, UniswapV2Pair$Abi()));
+        univ2Config.chainIdToAbiEnumerable.set(Chains.Base, chainAbi(Chains.Base, UniswapV2Pair$Abi()));
+        univ3Config.chainIdToAbiEnumerable.set(Chains.Ethereum, chainAbi(Chains.Ethereum, UniswapV3Pool$Abi()));
+        univ3Config.chainIdToAbiEnumerable.set(Chains.Base, chainAbi(Chains.Base, UniswapV3Pool$Abi()));
+        crocSwapConfig.chainIdToAbiEnumerable.set(Chains.Ethereum, chainAbi(Chains.Ethereum, HotProxy$Abi()));
+        crocSwapConfig.chainIdToAbiEnumerable.set(Chains.Base, chainAbi(Chains.Base, HotProxy$Abi()));
+        curveOldExchangeConfig.chainIdToAbiEnumerable.set(
+            Chains.Ethereum, chainAbi(Chains.Ethereum, OldTokenExchange$Abi())
+        );
+        curveExchangeConfig.chainIdToAbiEnumerable.set(Chains.Ethereum, chainAbi(Chains.Ethereum, TokenExchange$Abi()));
+        curveOldExchangeConfig.chainIdToAbiEnumerable.set(Chains.Base, chainAbi(Chains.Base, OldTokenExchange$Abi()));
+        curveExchangeConfig.chainIdToAbiEnumerable.set(Chains.Base, chainAbi(Chains.Base, TokenExchange$Abi()));
+        balancerV2Config.chainIdToAbiEnumerable.set(Chains.Ethereum, chainAbi(Chains.Ethereum, Vault$Abi()));
+        balancerV2Config.chainIdToAbiEnumerable.set(Chains.Base, chainAbi(Chains.Base, Vault$Abi()));
+        maverickV1Config.chainIdToAbiEnumerable.set(Chains.Ethereum, chainAbi(Chains.Ethereum, MaverickPool$Abi()));
+        maverickV1Config.chainIdToAbiEnumerable.set(Chains.Base, chainAbi(Chains.Base, MaverickPool$Abi()));
+        uniswapXConfig.chainIdToAbiEnumerable.set(Chains.Ethereum, chainAbi(Chains.Ethereum, Reactor$Abi()));
+        uniswapXConfig.chainIdToAbiEnumerable.set(Chains.Base, chainAbi(Chains.Base, Reactor$Abi()));
+        uniswapXConfig.chainIdToAbiEnumerable.set(Chains.Unichain, chainAbi(Chains.Unichain, Reactor$Abi()));
+        dodoV2Config.chainIdToAbiEnumerable.set(Chains.Ethereum, chainAbi(Chains.Ethereum, DODOSwap$Abi()));
+        dodoV2Config.chainIdToAbiEnumerable.set(Chains.Base, chainAbi(Chains.Base, DODOSwap$Abi()));
+        wooFiConfig.chainIdToAbiEnumerable.set(Chains.Ethereum, chainAbi(Chains.Ethereum, WooSwap$Abi()));
+        wooFiConfig.chainIdToAbiEnumerable.set(Chains.Base, chainAbi(Chains.Base, WooSwap$Abi()));
+        fluidDexConfig.chainIdToAbiEnumerable.set(Chains.Ethereum, chainAbi(Chains.Ethereum, FluidDexT1$Abi()));
+        fluidDexConfig.chainIdToAbiEnumerable.set(Chains.Base, chainAbi(Chains.Base, FluidDexT1$Abi()));
+        maverickV2Config.chainIdToAbiEnumerable.set(Chains.Ethereum, chainAbi(Chains.Ethereum, MaverickV2Pool$Abi()));
+        maverickV2Config.chainIdToAbiEnumerable.set(Chains.Base, chainAbi(Chains.Base, MaverickV2Pool$Abi()));
+        zeroExSettlerConfig.chainIdToAbiEnumerable.set(Chains.Ethereum, chainAbi(Chains.Ethereum, MainnetSettler$Abi()));
 
         // per protocol triggers
         psmConfig.triggers = [psmListener.PSM$triggerOnBuyGemFunction(), psmListener.PSM$triggerOnSellGemFunction()];
@@ -270,14 +280,16 @@ contract Triggers is BaseTriggers {
     }
 
     function addTriggerForProtocol(ProtocolConfigAbi storage config) internal {
-        for (uint256 i = 0; i < config.chains.length; i++) {
-            addTriggers(config.chainIdToAbi[config.chains[i]], config.triggers);
+        for (uint256 i = 0; i < config.chainIdToAbiEnumerable.length(); i++) {
+            (, ChainIdAbi memory _abi) = config.chainIdToAbiEnumerable.getAtIndex(i);
+            addTriggers(_abi, config.triggers);
         }
     }
 
     function addTriggerForProtocol(ProtocolConfigAddress storage config) internal {
         for (uint256 i = 0; i < config.chains.length; i++) {
-            addTriggers(config.chainIdToAddress[config.chains[i]], config.triggers);
+            (, ChainIdContract memory _contract) = config.chainIdToAddressEnumerable.getAtIndex(i);
+            addTriggers(_contract, config.triggers);
         }
     }
 
