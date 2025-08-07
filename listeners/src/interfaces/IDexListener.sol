66f76138bddd8a45c4158956c2c83d9cd7f0aabf
Tal Vaizman <tal@comitylabs.com>
2025-08-08 02:21:37 +0300

    remove DESC

--- listeners/src/interfaces/IDexListener.sol
+++ listeners/src/interfaces/IDexListener.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {DexTradeData} from "../types/DexTrades.sol";
 
 interface IDexListener {
-    /// @custom:index dex_trades_chainid_blocktimestamp_idx BTREE (chainId, blockTimestamp DESC);
-    /// @custom:index dex_trades_blocktimestamp_idx BTREE (blockTimestamp DESC);
+    /// @custom:index dex_trades_chainid_blocktimestamp_idx BTREE (chainId, blockTimestamp);
+    /// @custom:index dex_trades_blocktimestamp_idx BTREE (blockTimestamp);
     event DexTrade(DexTradeData);
 }
